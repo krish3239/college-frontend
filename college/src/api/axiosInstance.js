32c544289ea4c90106a908070e97fcf80984d542
krish3239 <krishbhardwaj3239@gmail.com>
2025-09-13 21:00:25 +0530

    the changes

--- college/src/api/axiosInstance.js
+++ college/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 // axiosInstance.js
 import axios from 'axios';
 
-const API_URL = "http://localhost:3000/api";
+const API_URL = "https://college-backend-beta.vercel.app/api";
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
